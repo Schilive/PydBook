67d781d3dbd98a1691692dbf5da60f54c7859a18
Gabriel Schilive <schilive100@gmail.com>
2022-09-04 01:20:29 -0300

    Small refactoring, added 'exit' button function. And, added the function to ask the user if they want to save their text before some action, if not yet saved.

--- PydBook.py
+++ PydBook.py
@@ -5,7 +5,7 @@ APP_TITLE = "PydBook"  # Name of the application, on the titles of the windows, 
 
 
 class MainUI(QtWidgets.QMainWindow):
-    """This is the main UI, which is the one shown when the app is started, and whereof everything else is son"""
+    """This is the main UI, which is the one shown when the app is started, and whereof everything else is son."""
 
     def __init__(self):
         super().__init__()
@@ -15,7 +15,7 @@ class MainUI(QtWidgets.QMainWindow):
         self.isSaveFile: bool = False  # Whether there is a saving file
         self.save_file: str = ""
 
-        self.saved: bool = False  # Whether the current text has been saved.
+        self.saved: bool = True  # Whether the current text has been saved, or whether it is blank.
         self.saved_text: str = ""  # This is a bad solution, better would be to save the changes done
 
         # UI Settings
@@ -30,8 +30,7 @@ class MainUI(QtWidgets.QMainWindow):
 
         self.text_editor = QtWidgets.QPlainTextEdit()
         self.text_editor.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOn)
-        self.text_editor.old_keyPressEvent = self.text_editor.keyPressEvent
-        self.text_editor.keyPressEvent = self.text_editor_keyPressEvent
+        self.text_editor.textChanged.connect(self.text_changed)
         self.setCentralWidget(self.text_editor)
 
         # Menu Bar
@@ -68,8 +67,11 @@ class MainUI(QtWidgets.QMainWindow):
         self.statusBar = QtWidgets.QStatusBar()
         self.setStatusBar(self.statusBar)
 
-    def text_editor_keyPressEvent(self, e):
-        self.text_editor.old_keyPressEvent(e)
+    def closeEvent(self, event: QtGui.QCloseEvent) -> None:
+        self.exit()
+
+    def text_changed(self):
+        """Called when the text, of 'self.text_editor', changes."""
 
         if self.text_editor.toPlainText() != self.saved_text:
             self.saved = False
@@ -92,12 +94,71 @@ class MainUI(QtWidgets.QMainWindow):
         self.isSaveFile = True
         self.save_file = file_src
         self.saved_text = self.text_editor.toPlainText()
+        self.saved = True
 
-
-        print(file_src)
         file_name = file_src.split("/")[-1]
         self.setWindowTitle(f"{file_name} — {APP_TITLE}")
 
+    def ask_if_wants_to_save(self, second_text: str = "") -> int:
+        """This is called for the user to decide whether they want to save the file, before some other action;
+        for example, opening a file. The variable 'second_text' adds a text to enlight the user. Returns the pressed
+        button (Yes, No, Cancel), and 0 if unsuccessful."""
+
+        # Building the Asking Box
+
+        asking_box = QtWidgets.QMessageBox(self)
+        asking_box.setWindowModality(QtCore.Qt.WindowModal)
+        asking_box.setWindowTitle(APP_TITLE)
+
+        warning_icon = QtWidgets.QMessageBox.Warning
+        asking_box.setIcon(warning_icon)
+
+        cancel_button = QtWidgets.QMessageBox.Cancel
+        not_save_button = QtWidgets.QMessageBox.No
+        save_button = QtWidgets.QMessageBox.Yes
+        asking_box.setStandardButtons(cancel_button | not_save_button | save_button)
+
+        asking_text: str
+        if self.isSaveFile:
+            save_file_name = self.save_file.split("/")[-1]
+            asking_text = f"Would you like to save {save_file_name}?"
+        else:
+            asking_text = "Would you like to save this file?"
+
+        asking_text += second_text
+        asking_box.setText(asking_text)
+
+        # Asking and Interpreting
+
+        pressed_button = asking_box.exec()  # Returns 0 if unsuccessful
+        return pressed_button
+
+    def save_warn_if_needed(second_text: str = ""):
+        """This is a decorator with the attribute 'second_text'. If the text has not been saved, tt asks if the user
+        wants to save the file before an action, which is the function the decorator wraps.
+
+        The attribute is directly concatenated to the asker box. If the user refuses, the action will continue normally;
+        if the user accepts, they will be asked to save the file and, then, the action will continue normally; if they
+        somehow cancel, nothing shall happen."""
+
+        def decorator(func):
+            def wrapper(self, *args, **kwargs):
+                if not self.saved:
+                    user_will = self.ask_if_wants_to_save(second_text)
+
+                    if user_will == 0 or user_will == QtWidgets.QMessageBox.Cancel:  # Canceled
+                        return
+                    elif user_will == QtWidgets.QMessageBox.No:  # Not save
+                        func(self, *args, **kwargs)
+                    elif user_will == QtWidgets.QMessageBox.Yes:  # Wants to save
+                        self.user_save()
+                        func(self, *args, **kwargs)
+                else:
+                    func(self, *args, **kwargs)
+            return wrapper
+        return decorator
+
+    @save_warn_if_needed(" Before opening a file..")
     def open(self):
         """Called for the user to open a file, altering the text to the text in the selected file, using UTF-8."""
 
@@ -113,10 +174,9 @@ class MainUI(QtWidgets.QMainWindow):
         file_selector.setLabelText(file_selector.FileType, "Type:")
 
         file_selector.setWindowTitle("Open a File")
-        file_selector_filter_1: str = "Text File (*.txt)"
-        file_selector_filter_2: str = "Any File (*)"
-        file_selector.setNameFilter(f"{file_selector_filter_1};;{file_selector_filter_2}")
-        file_selector.setViewMode(QtWidgets.QFileDialog.List)
+        text_filter: str = "Text File (*.txt)"
+        any_file_filter: str = "Any File (*)"
+        file_selector.setNameFilter(f"{text_filter};;{any_file_filter}")  # Filters are separated by ;;
 
         result = file_selector.exec()  # Returns 1 if file was selected, 0 otherwise; for example, 'cancel' is pressed
 
@@ -141,7 +201,7 @@ class MainUI(QtWidgets.QMainWindow):
             self.set_saving_file(file_selected)
 
     def user_save(self):
-        """Called when the users pressed the 'save' button."""
+        """Called when the users want to 'save' the text."""
 
         if not self.isSaveFile:
             self.save_as()
@@ -155,7 +215,7 @@ class MainUI(QtWidgets.QMainWindow):
         # File Selector Widget
 
         file_selector = QtWidgets.QFileDialog(self)
-        file_selector.setFileMode(QtWidgets.QFileDialog.AnyFile)  # Only one file is to be selected
+        file_selector.setFileMode(QtWidgets.QFileDialog.AnyFile)  # Only one file is to be selected, existing or not
 
         file_selector.setLabelText(file_selector.Accept, "Save")
         file_selector.setLabelText(file_selector.Reject, "Close")
@@ -166,7 +226,7 @@ class MainUI(QtWidgets.QMainWindow):
         file_selector.setWindowTitle("Save As")
         text_filter: str = "Text File (*.txt)"
         any_file_filter: str = "Any File (*)"
-        file_selector.setNameFilter(f"{text_filter};;{any_file_filter}")
+        file_selector.setNameFilter(f"{text_filter};;{any_file_filter}")  # Filter are separated by ;;
         file_selector.setViewMode(QtWidgets.QFileDialog.List)
 
         # File Selection
@@ -207,8 +267,9 @@ class MainUI(QtWidgets.QMainWindow):
             file.close()
             self.set_saving_file(file_src)
 
+    @save_warn_if_needed(" Before closing.")
     def exit(self):
-        print("Exit")
+        self.close()
 
 
 class WarningMessage(QtWidgets.QMessageBox):
@@ -232,7 +293,7 @@ class WarningMessage(QtWidgets.QMessageBox):
 
 class ErrorMessage(QtWidgets.QMessageBox):
     """This class is called when a critical error happens. After its button is clicked, or it is closed, its parent
-    is closed. Normally, the parent is MainUI, except for when an instance of MainUI is formed."""
+    is closed. Normally, the parent is MainUI, except for when an instance of MainUI is being formed."""
 
     def __init__(self, parent=None, exception: Exception = None):
         super().__init__(parent=parent)
@@ -245,7 +306,7 @@ class ErrorMessage(QtWidgets.QMessageBox):
         error_icon = QtWidgets.QMessageBox.Critical
         self.setIcon(error_icon)
 
-        error_text = "An exception was raised" + "\n" + (f"{exception.__str__()}" if exception else "")
+        error_text = "An exception was raised" + (f"\n{exception.__str__()}" if exception else "")
         self.setText(error_text)
 
     def end_all(self):
