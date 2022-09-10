035e6556f9c462f963da5720a2379499f9448980
Gabriel Schilive <schilive100@gmail.com>
2022-09-10 20:31:56 -0300

    Two languages are now supported. The default langauge is the default langauge of the system.

--- PydBook.py
+++ PydBook.py
@@ -1,18 +1,54 @@
 from PySide6 import QtCore, QtWidgets, QtGui
-import math
+import math  # Only for floor function
+import locale  # For knowing the user's language
+
+
+def get_language() -> str:
+    """Gets the language to be used. If the user's language is supported, their language code (e.g. "en") is returned,
+    otherwise the standard language is returned."""
+
+    supported_languages: set[str] = {"en", "pt"}
+    standard_lang: str = "en"
+
+    lang_local: str = locale.getdefaultlocale()[0]  # If the user is in Spain and using English, = en_ES
+    lang: str = lang_local.split("_")[0]
+
+    if lang in supported_languages:
+        return lang
+    else:
+        return standard_lang
+
 
 # Constants
 APP_TITLE = "PydBook"  # Name of the application, on the titles of the windows, for example.
+LANGUAGE = get_language()
 
 
 class MainUI(QtWidgets.QMainWindow):
     """This is the main UI, which is the one shown when the app is started, and whereof everything else is son."""
+
+    def texts(self):
+        """It is generator, containing the texts on the right langauge. This generator is used for naming in the
+        __init__ function. It is a generator to save memory."""
+
+        src = f"lang/{LANGUAGE}.txt"
+
+        with open(file=src, mode="r", encoding="utf-8") as file:
+            while True:
+                text = file.readline()
+
+                if text == "" or text == "\n":
+                    break
+
+                yield text[:-1]  # The last character is "\n"
 
     def __init__(self):
         super().__init__()
 
         # Application Variables
         self.stylesheet_file = "./style/main.stylesheet"
+
+        self.standard_title = ""  # Standard title, before the text is associated with a file
 
         self.isSaveFile: bool = False  # Whether there is a saving file
         self.save_file: str = ""
@@ -26,8 +62,14 @@ class MainUI(QtWidgets.QMainWindow):
         self.maximum_zoom = 500
         self.minimum_zoom = 10
 
+        # Temporary Variable
+
+        # It contains the texts on the right language, which will be used here for naming
+        texts = self.texts()
+        self.standard_title = next(texts)
+
         # UI Settings
-        self.setWindowTitle(f"Untitled — {APP_TITLE}")
+        self.setWindowTitle(f"{self.standard_title} — {APP_TITLE}")
 
         self.update_style()
 
@@ -40,49 +82,49 @@ class MainUI(QtWidgets.QMainWindow):
 
         # Menu Bar
 
-        self.menuBar_file = self.menuBar().addMenu("&File")
+        self.menuBar_file = self.menuBar().addMenu(next(texts))
         self.menuBar_file.setWindowFlags(self.menuBar_file.windowFlags() | QtCore.Qt.NoDropShadowWindowHint)
 
-        self.open_action = QtGui.QAction("&Open")
+        self.open_action = QtGui.QAction(next(texts))
         self.open_action.setShortcut("Ctrl+O")
         self.open_action.triggered.connect(self.open)
         self.menuBar_file.addAction(self.open_action)
 
         self.menuBar_file.addSeparator()
 
-        self.save_action = QtGui.QAction("&Save")
+        self.save_action = QtGui.QAction(next(texts))
         self.save_action.setShortcut("Ctrl+S")
         self.save_action.triggered.connect(self.user_save)
         self.menuBar_file.addAction(self.save_action)
 
-        self.save_as_action = QtGui.QAction("Save &As")
+        self.save_as_action = QtGui.QAction(next(texts))
         self.save_as_action.setShortcut("Ctrl+Shift+S")
         self.save_as_action.triggered.connect(self.save_as)
         self.menuBar_file.addAction(self.save_as_action)
 
         self.menuBar_file.addSeparator()
 
-        self.exit_action = QtGui.QAction("E&xit")
+        self.exit_action = QtGui.QAction(next(texts))
         self.exit_action.setShortcut("Ctrl+E")
         self.exit_action.triggered.connect(self.exit)
         self.menuBar_file.addAction(self.exit_action)
 
-        self.menuBar_view = self.menuBar().addMenu("&View")
+        self.menuBar_view = self.menuBar().addMenu(next(texts))
         self.menuBar_file.setWindowFlags(self.menuBar_file.windowFlags() | QtCore.Qt.NoDropShadowWindowHint)
 
-        self.zoomIn_action = QtGui.QAction("Zoom In")
+        self.zoomIn_action = QtGui.QAction(next(texts))
         self.zoomIn_action.setShortcut("Ctrl++")
         self.zoomIn_action.triggered.connect(self.zoom_in)
         self.menuBar_view.addAction(self.zoomIn_action)
 
-        self.zoomOut_action = QtGui.QAction("Zoom Out")
+        self.zoomOut_action = QtGui.QAction(next(texts))
         self.zoomOut_action.setShortcut("Ctrl+-")
         self.zoomOut_action.triggered.connect(self.zoom_out)
         self.menuBar_view.addAction(self.zoomOut_action)
 
         self.menuBar_view.addSeparator()
 
-        self.noZoom_action = QtGui.QAction("No Zoom")
+        self.noZoom_action = QtGui.QAction(next(texts))
         self.noZoom_action.triggered.connect(self.no_zoom)
         self.menuBar_view.addAction(self.noZoom_action)
 
@@ -114,7 +156,7 @@ class MainUI(QtWidgets.QMainWindow):
                 file_name = self.save_file.split("/")[-1]
                 self.setWindowTitle(f"{file_name}* — {APP_TITLE}")
             else:
-                self.setWindowTitle(f"Untitled* — {APP_TITLE}")
+                self.setWindowTitle(f"{self.standard_title}* — {APP_TITLE}")
         else:
             self.saved = True
 
@@ -122,7 +164,7 @@ class MainUI(QtWidgets.QMainWindow):
                 file_name = self.save_file.split("/")[-1]
                 self.setWindowTitle(f"{file_name} — {APP_TITLE}")
             else:
-                self.setWindowTitle(f"Untitled — {APP_TITLE}")
+                self.setWindowTitle(f"{self.standard_title} — {APP_TITLE}")
 
     def set_saving_file(self, file_src: str):
         """Called when saved or a file is opened. It updates whether the application has a file it can save to."""
@@ -155,11 +197,27 @@ class MainUI(QtWidgets.QMainWindow):
         asking_box.setStandardButtons(cancel_button | not_save_button | save_button)
 
         asking_text: str
-        if self.isSaveFile:
-            save_file_name = self.save_file.split("/")[-1]
-            asking_text = f"Would you like to save {save_file_name}?"
-        else:
-            asking_text = "Would you like to save this file?"
+        match LANGUAGE:
+            case "en":
+                asking_box.button(cancel_button).setText("Cancel")
+                asking_box.button(not_save_button).setText("No")
+                asking_box.button(save_button).setText("Yes")
+
+                if self.isSaveFile:
+                    save_file_name = self.save_file.split("/")[-1]
+                    asking_text = f"Would you like to save {save_file_name}?"
+                else:
+                    asking_text = "Would you like to save this text?"
+            case "pt":
+                asking_box.button(cancel_button).setText("Cancelar")
+                asking_box.button(not_save_button).setText("Não")
+                asking_box.button(save_button).setText("Sim")
+
+                if self.isSaveFile:
+                    save_file_name = self.save_file.split("/")[-1]
+                    asking_text = f"Você gostaria de salvar {save_file_name}?"
+                else:
+                    asking_text = "Você gostaria de salvar este texto?"
 
         asking_text += second_text
         asking_box.setText(asking_text)
@@ -169,18 +227,19 @@ class MainUI(QtWidgets.QMainWindow):
         pressed_button = asking_box.exec()  # Returns 0 if unsuccessful
         return pressed_button
 
-    def save_warn_if_needed(second_text: str = ""):
+    def save_warn_if_needed(second_texts: dict[str, str] = ""):
         """This is a decorator with the attribute 'second_text'. If the text has not been saved, tt asks if the user
         wants to save the file before an action, which is the function the decorator wraps.
 
-        The attribute is directly concatenated to the asker box. If the user refuses, the action will continue normally;
-        if the user accepts, they will be asked to save the file and, then, the action will continue normally; if they
-        somehow cancel, nothing shall happen."""
+        The attribute is a dictionary with the languge code (e.g. "EN") pointing to the message on the appropriate
+        language; the message is directly concatenated to the asker box. If the user refuses, the action will continue
+        normally; if the user accepts, they will be asked to save the file and, then, the action will continue
+        normally; if they somehow cancel, nothing shall happen."""
 
         def decorator(func):
             def wrapper(self, *args, **kwargs):
                 if not self.saved:
-                    user_will = self.ask_if_wants_to_save(second_text)
+                    user_will = self.ask_if_wants_to_save(second_texts[LANGUAGE])
 
                     if user_will == 0 or user_will == QtWidgets.QMessageBox.Cancel:  # Canceled
                         return
@@ -194,7 +253,8 @@ class MainUI(QtWidgets.QMainWindow):
             return wrapper
         return decorator
 
-    @save_warn_if_needed(" Before opening a file..")
+    @save_warn_if_needed({"en": " Before opening a file.",
+                          "pt": " Antes de abrir o arquivo."})
     def open(self):
         """Called for the user to open a file, altering the text to the text in the selected file, using UTF-8."""
 
@@ -203,15 +263,26 @@ class MainUI(QtWidgets.QMainWindow):
         file_selector = QtWidgets.QFileDialog(self)
         file_selector.setFileMode(QtWidgets.QFileDialog.ExistingFile)  # Only one file is to be selected
 
-        file_selector.setLabelText(file_selector.Accept, "Open")
-        file_selector.setLabelText(file_selector.Reject, "Close")
-        file_selector.setLabelText(file_selector.FileName, "Name:")
-        file_selector.setLabelText(file_selector.LookIn, "Look in:")
-        file_selector.setLabelText(file_selector.FileType, "Type:")
+        match LANGUAGE:
+            case "en":
+                file_selector.setLabelText(file_selector.Accept, "Open")
+                file_selector.setLabelText(file_selector.Reject, "Close")
+                file_selector.setLabelText(file_selector.FileName, "Name:")
+                file_selector.setLabelText(file_selector.LookIn, "Look in:")
+                file_selector.setLabelText(file_selector.FileType, "Type:")
+                file_selector.setWindowTitle("Open a File")
+                text_filter: str = "Text File (*.txt)"
+                any_file_filter: str = "Any File (*)"
+            case "pt":
+                file_selector.setLabelText(file_selector.Accept, "Abrir")
+                file_selector.setLabelText(file_selector.Reject, "Fechar")
+                file_selector.setLabelText(file_selector.FileName, "Nome:")
+                file_selector.setLabelText(file_selector.LookIn, "Em:")
+                file_selector.setLabelText(file_selector.FileType, "Tipo:")
+                file_selector.setWindowTitle("Abrir um Arquivo")
+                text_filter: str = "Documento de Texto (*.txt)"
+                any_file_filter: str = "Todos os Arquivos (*)"
 
-        file_selector.setWindowTitle("Open a File")
-        text_filter: str = "Text File (*.txt)"
-        any_file_filter: str = "Any File (*)"
         file_selector.setNameFilter(f"{text_filter};;{any_file_filter}")  # Filters are separated by ;;
 
         result = file_selector.exec()  # Returns 1 if file was selected, 0 otherwise; for example, 'cancel' is pressed
@@ -227,7 +298,13 @@ class MainUI(QtWidgets.QMainWindow):
             file = open(file_selected, "r", encoding="utf-8")
             text = file.read()
         except Exception:
-            warning_box = WarningMessage(self, text=f"{APP_TITLE} could not open the file: {file_selected}.")
+            match LANGUAGE:
+                case "en":
+                    warning_message = f"{APP_TITLE} could not open the file: {file_selected}."
+                case "pt":
+                    warning_message = f"{APP_TITLE} não pôde abriro o arquivo: {file_selected}."
+
+            warning_box = WarningMessage(self, text=warning_message)
             warning_box.exec()
             return
         else:
@@ -253,15 +330,26 @@ class MainUI(QtWidgets.QMainWindow):
         file_selector = QtWidgets.QFileDialog(self)
         file_selector.setFileMode(QtWidgets.QFileDialog.AnyFile)  # Only one file is to be selected, existing or not
 
-        file_selector.setLabelText(file_selector.Accept, "Save")
-        file_selector.setLabelText(file_selector.Reject, "Close")
-        file_selector.setLabelText(file_selector.FileName, "Name:")
-        file_selector.setLabelText(file_selector.LookIn, "Look in:")
-        file_selector.setLabelText(file_selector.FileType, "Type:")
+        match LANGUAGE:
+            case "en":
+                file_selector.setLabelText(file_selector.Accept, "Save")
+                file_selector.setLabelText(file_selector.Reject, "Close")
+                file_selector.setLabelText(file_selector.FileName, "Name:")
+                file_selector.setLabelText(file_selector.LookIn, "Look in:")
+                file_selector.setLabelText(file_selector.FileType, "Type:")
+                file_selector.setWindowTitle("Save As")
+                text_filter: str = "Text File (*.txt)"
+                any_file_filter: str = "Any File (*)"
+            case "pt":
+                file_selector.setLabelText(file_selector.Accept, "Salvar")
+                file_selector.setLabelText(file_selector.Reject, "Fechar")
+                file_selector.setLabelText(file_selector.FileName, "Nome:")
+                file_selector.setLabelText(file_selector.LookIn, "Em")
+                file_selector.setLabelText(file_selector.FileType, "Tipo:")
+                file_selector.setWindowTitle("Salvar Como")
+                text_filter: str = "Documento de Texto (*.txt)"
+                any_file_filter: str = "Todos os Arquivos (*)"
 
-        file_selector.setWindowTitle("Save As")
-        text_filter: str = "Text File (*.txt)"
-        any_file_filter: str = "Any File (*)"
         file_selector.setNameFilter(f"{text_filter};;{any_file_filter}")  # Filter are separated by ;;
         file_selector.setViewMode(QtWidgets.QFileDialog.List)
 
@@ -296,14 +384,20 @@ class MainUI(QtWidgets.QMainWindow):
             text = self.text_editor.toPlainText()
             file.write(text)
         except Exception:
-            warning_text = f"{APP_TITLE} could not save the text at {file_src}."
+            match LANGUAGE:
+                case "en":
+                    warning_text = f"{APP_TITLE} could not save the text at {file_src}."
+                case "pt":
+                    warning_text = f"{APP_TITLE} não pôde salvar o texto em {file_src}."
+
             warning_box = WarningMessage(self, text=warning_text)
             warning_box.exec()
         else:
             file.close()
             self.set_saving_file(file_src)
 
-    @save_warn_if_needed(" Before closing.")
+    @save_warn_if_needed({"en": " Before closing.",
+                          "pt": f" Antes de fechar o {APP_TITLE}."})
     def exit(self):
         """Called when the user wants to exit."""
 
@@ -372,7 +466,12 @@ class ErrorMessage(QtWidgets.QMessageBox):
         error_icon = QtWidgets.QMessageBox.Critical
         self.setIcon(error_icon)
 
-        error_text = "An exception was raised" + (f"\n{exception.__str__()}" if exception else "")
+        match LANGUAGE:
+            case "en":
+                error_text = "An exception was raised" + (f"\n{exception.__str__()}." if exception else "")
+            case "pt":
+                error_text = "Uma exceção foi gerada" + (f"\n{exception.__str__()}." if exception else "")
+
         self.setText(error_text)
 
     def end_all(self):
