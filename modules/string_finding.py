85ccd368932a6924bbec2e1e3abd01a5c733661c
Gabriel Schilive <schilive100@gmail.com>
2022-10-19 22:23:27 -0300

    Substitution function and blocky occurrences

--- modules/string_finding.py
+++ modules/string_finding.py
@@ -1,8 +1,12 @@
 """Deals with finding strings inside strings, and substituting those strings"""""
 
 
-def find_string(find: str, text: str) -> list[int]:
-    """Finds all exact occurrences the substring sub inside 'text'. Returns the first indexes of the occurrences"""
+def find_occurrences(text: str, find: str, block: bool = False) -> list[int]:
+    """Finds all occurrences the substring sub inside 'text'. Returns the first indexes of the occurrences.
+
+    Arguments:
+        block - If two occurrences overlap, only the first is considered, creating "blocky" occurrences.
+    """
 
     first_indexes: list[int] = []
 
@@ -10,13 +14,26 @@ def find_string(find: str, text: str) -> list[int]:
         return []
 
     last_possible_index: int = len(text) - len(find)
-    for first_char_index in range(0, last_possible_index + 1):
+    first_char_index: int = 0
+    while first_char_index < last_possible_index + 1:
         sub_text = text[first_char_index: first_char_index + len(find)]
 
         if sub_text == find:
             first_indexes.append(first_char_index)
+            first_char_index += len(find) * block
+            continue
+
+        first_char_index += 1
 
     return first_indexes
 
 
-print(find_string("eu", "eu sou eu, o que é demais!eu"))
+def substitute(text: str, replacing: str, replacement: str):
+    list_indexes: list[int] = find_occurrences(text, replacing, block=True)
+    list_indexes.reverse()
+
+    for first_index in list_indexes:
+        last_index: int = first_index + (len(replacing) - 1)
+        text = text[:first_index] + replacement + text[last_index + 1:]
+
+    return text
